c4be05cbaa0d58bf4946b0c544566231a3e3fcc8
Jose Manuel Rosa <josemanuel.rosa@bq.com>
2014-10-06 15:52:03 +0200

    Indentation and tabs

--- src/blocks/advanced_conversion/advanced_conversion.js
+++ src/blocks/advanced_conversion/advanced_conversion.js
@@ -7,13 +7,13 @@
  * @return {String} Code generated with block parameters
  */
 Blockly.Arduino.advanced_conversion = function() {
-	var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_NONE) || '10';
-	var convertion=this.getFieldValue('CONV');
-	var code = JST['advanced_conversion']({
-		'value_num' : value_num,
-		'convertion' : convertion
-	});
-	return [code, Blockly.Arduino.ORDER_ATOMIC];
+    var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_NONE) || '10';
+    var convertion=this.getFieldValue('CONV');
+    var code = JST['advanced_conversion']({
+        'value_num' : value_num,
+        'convertion' : convertion
+    });
+    return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
 /**
@@ -21,25 +21,25 @@ Blockly.Arduino.advanced_conversion = function() {
  * @type {Object}
  */
 Blockly.Blocks.advanced_conversion = {
-	category: Blockly.LANG_CATEGORY_ADVANCED,
-	helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/serial_println',
-	 /**
-	  * advanced_conversion initialization
-	  */
-	init: function() {
-		this.setColour(Blockly.LANG_COLOUR_ADVANCED);
-		this.appendDummyInput('')
-			.appendField('Convert')
-			.appendField(new Blockly.FieldDropdown([
-				['Decimal', 'DEC'],
-				['Hexadecimal', 'HEX'],
-				['Octal', 'OCT'],
-				['Binary', 'BIN']
-			]), 'CONV');
-		this.appendValueInput('NUM', Number)
-			.appendField('value')
-			.setCheck(Number);
-		this.setOutput(true, Number);
-		this.setTooltip('Base convert a number');
-	}
+    category: Blockly.LANG_CATEGORY_ADVANCED,
+    helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/serial_println',
+    /**
+     * advanced_conversion initialization
+     */
+    init: function() {
+        this.setColour(Blockly.LANG_COLOUR_ADVANCED);
+        this.appendDummyInput('')
+        .appendField('Convert')
+        .appendField(new Blockly.FieldDropdown([
+            ['Decimal', 'DEC'],
+            ['Hexadecimal', 'HEX'],
+            ['Octal', 'OCT'],
+            ['Binary', 'BIN']
+        ]), 'CONV');
+        this.appendValueInput('NUM', Number)
+        .appendField('value')
+        .setCheck(Number);
+        this.setOutput(true, Number);
+        this.setTooltip('Base convert a number');
+    }
 };
