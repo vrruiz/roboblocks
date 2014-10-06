1c4a1a5823fe77fa0657761bd2e7e2dd4ce484d7
Jose Manuel Rosa <josemanuel.rosa@bq.com>
2014-10-06 16:32:48 +0200

    zum tags

--- src/blocks/zum_led/zum_led.js
+++ src/blocks/zum_led/zum_led.js
@@ -6,53 +6,49 @@
  * zum_led code generation
  * @return {String} Code generated with block parameters
  */
-
-
 Blockly.Arduino.zum_led = function() {
-	var dropdown_pin = this.getFieldValue('PIN');
-	var dropdown_stat = this.getFieldValue('STAT');
+    var dropdown_pin = this.getFieldValue('PIN');
+    var dropdown_stat = this.getFieldValue('STAT');
 
 
-	Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
-		'dropdown_pin': dropdown_pin,
-		'dropdown_stat': dropdown_stat
-	});
+    Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+        'dropdown_pin': dropdown_pin,
+        'dropdown_stat': dropdown_stat
+    });
 
-	var code = JST['zum_led']({
-		'dropdown_pin': dropdown_pin,
-		'dropdown_stat': dropdown_stat
-	});
+    var code = JST['zum_led']({
+        'dropdown_pin': dropdown_pin,
+        'dropdown_stat': dropdown_stat
+    });
 
-	return code;
+    return code;
 };
 
 /**
  * zum_led block definition
  * @type {Object}
  */
-
-
-
 Blockly.Blocks.zum_led = {
-	category: Blockly.LANG_CATEGORY_ZUM,
-	helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/zum_led',
-	 /**
-	  * zum_led initialization
-	  */
-	init: function() {
-		this.setColour(Blockly.LANG_COLOUR_ZUM);
-		this.appendDummyInput('')
-			.appendField('LED')
-			.appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom))
-			.appendField('PIN#')
-			.appendField(new Blockly.FieldDropdown(profiles.default.digital), 'PIN')
-			.appendField('state')
-			.appendField(new Blockly.FieldDropdown([
-				['ON', 'HIGH'],
-				['OFF', 'LOW']
-			]), 'STAT');
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setTooltip('bq LED');
-	}
+    category: Blockly.LANG_CATEGORY_ZUM,
+    tags: ['bq', 'zum', 'led'],
+    helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/zum_led',
+    /**
+    * zum_led initialization
+    */
+    init: function() {
+        this.setColour(Blockly.LANG_COLOUR_ZUM);
+        this.appendDummyInput('')
+        .appendField('LED')
+        .appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom))
+        .appendField('PIN#')
+        .appendField(new Blockly.FieldDropdown(profiles.default.digital), 'PIN')
+        .appendField('state')
+        .appendField(new Blockly.FieldDropdown([
+            ['ON', 'HIGH'],
+            ['OFF', 'LOW']
+        ]), 'STAT');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('bq LED');
+    }
 };
--- src/blocks/zum_photoresistor/zum_photoresistor.js
+++ src/blocks/zum_photoresistor/zum_photoresistor.js
@@ -7,14 +7,14 @@
  * @return {String} Code generated with block parameters
  */
 Blockly.Arduino.zum_photoresistor = function() {
-	var dropdown_pin = this.getFieldValue('PIN');
+    var dropdown_pin = this.getFieldValue('PIN');
 
-	var code = JST['zum_photoresistor']({
-		'dropdown_pin': dropdown_pin
-	});
+    var code = JST['zum_photoresistor']({
+        'dropdown_pin': dropdown_pin
+    });
 
-//	code=code.substring(0,code.length-1);
-	return [code, Blockly.Arduino.ORDER_ATOMIC];
+    //  code=code.substring(0,code.length-1);
+    return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
 
@@ -23,19 +23,20 @@ Blockly.Arduino.zum_photoresistor = function() {
  * @type {Object}
  */
 Blockly.Blocks.zum_photoresistor = {
-	category: Blockly.LANG_CATEGORY_ZUM,
-	helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/zum_photoresistor',
-	 /**
-	  * zum_photoresistor initialization
-	  */
-	init: function() {
-		this.setColour(Blockly.LANG_COLOUR_ZUM);
-		this.appendDummyInput('')
-			.appendField('Photoresistor')
-			.appendField(new Blockly.FieldImage('img/blocks/zum05.png', 208 * options.zoom, 126 * options.zoom))
-				.appendField('PIN#')
-				.appendField(new Blockly.FieldDropdown(profiles.default.analog), 'PIN');
-		this.setOutput(true, Number);
-		this.setTooltip('Output the value of the photoresistor.');
-	}
+    category: Blockly.LANG_CATEGORY_ZUM,
+    tags: ['bq', 'zum', 'photoresistor'],
+    helpUrl: 'http://github.com/bq/roboblock/tree/master/src/blocks/zum_photoresistor',
+    /**
+    * zum_photoresistor initialization
+    */
+    init: function() {
+        this.setColour(Blockly.LANG_COLOUR_ZUM);
+        this.appendDummyInput('')
+        .appendField('Photoresistor')
+        .appendField(new Blockly.FieldImage('img/blocks/zum05.png', 208 * options.zoom, 126 * options.zoom))
+        .appendField('PIN#')
+        .appendField(new Blockly.FieldDropdown(profiles.default.analog), 'PIN');
+        this.setOutput(true, Number);
+        this.setTooltip('Output the value of the photoresistor.');
+    }
 };
