3b87628702dfafd802ba87213d9b491d8394f0f6
irenesanz <irenesanznieto@gmail.com>
2014-10-15 10:01:16 +0200

    cleaning bugs - bq_bat

--- src/blocks/bq_bat/bq_bat.js
+++ src/blocks/bq_bat/bq_bat.js
@@ -7,8 +7,8 @@
  * @return {String} Code generated with block parameters
  */
 Blockly.Arduino.bq_bat = function() {
-    var red_pin = this.getFieldValue('RED PIN');
-    var blue_pin = this.getFieldValue('BLUE PIN');
+    var red_pin = Blockly.Arduino.valueToCode(this, 'RED PIN');
+    var blue_pin = Blockly.Arduino.valueToCode(this, 'BLUE PIN');
 
     Blockly.Arduino.setups_['setup_bq_bat_']=JST['bq_bat_setups']({
         'red_pin': red_pin,
