25c968b666c1eb06a7a9f3e3ff1a8d1628409a93
irenesanznieto <irenesanznieto@gmail.com>
2014-10-16 17:17:18 +0200

    fixed bug bt_send

--- src/blocks/bq_bluetooth_send/bq_bluetooth_send.js
+++ src/blocks/bq_bluetooth_send/bq_bluetooth_send.js
@@ -9,7 +9,10 @@
 
 Blockly.Arduino.bq_bluetooth_send = function() {
     var statement_send = Blockly.Arduino.valueToCode(this,  'SNT', Blockly.Arduino.ORDER_ATOMIC);
-    statement_send=statement_send.replace('&quot;', '"');
+    statement_send=statement_send.replace(/&quot;/g,'"');
+
+    console.log('aaaaaaaaaaaaaaaa', statement_send);
+
     var code = JST['bq_bluetooth_send']({
         'statement_send' : statement_send
     });
