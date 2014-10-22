775a60c2f367bd46afe4a657760cccd570a47e4f
irenesanznieto <irene.sanz@bq.com>
2014-10-22 18:37:01 +0200

    fixed procedures tests

--- test/spec/procedures_defreturn.js
+++ test/spec/procedures_defreturn.js
@@ -12,7 +12,7 @@
 				expect( true ).to.equal( true );
 				var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'procedures_defreturn');
 
-				assert.equal(Blockly.Arduino.workspaceToCode(), 'void  () {\n    }\n\nvoid setup()\n{\n\n}\n\n\nvoid loop()\n{\n\n}');
+				assert.equal(Blockly.Arduino.workspaceToCode(), 'void function () {\n    }\n\nvoid setup()\n{\n\n}\n\n\nvoid loop()\n{\n\n}');
 			});
 		});
 	});
