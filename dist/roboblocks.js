1ba07ab7a19da5780a3b835ea82ca32910abd65a
Irene Sanz <irene.sanz@bq.com>
2015-01-08 13:14:01 +0100

    modified zum_led to put the pinmode in the loop when using a variable to define the pins

--- dist/roboblocks.js
+++ dist/roboblocks.js
@@ -8962,7 +8962,6 @@
         // Source: src/blocks/zum_led/zum_led.js
         /* global Blockly, options, JST, RoboBlocks */
         /* jshint sub:true */
-
         /**
          * zum_led code generation
          * @return {String} Code generated with block parameters
@@ -8970,21 +8969,38 @@
         Blockly.Arduino.zum_led = function() {
             var dropdown_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC) || '';
             var dropdown_stat = this.getFieldValue('STAT');
-
-
-            Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+            var code = '';
+            if (this.childBlocks_ !== undefined) {
+                var pin_block = [];
+                for (var i in this.childBlocks_) {
+                    if (this.childBlocks_[i].type === 'variables_get' || this.childBlocks_[i].type === 'math_number') {
+                        pin_block.push(this.childBlocks_[i].type);
+                    }
+                }
+                if (pin_block[0] === 'variables_get') {
+                    code += JST['zum_led_setups']({
+                        'dropdown_pin': dropdown_pin,
+                        'dropdown_stat': dropdown_stat
+                    });
+                }
+                if (pin_block[0] === 'math_number') {
+                    Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+                        'dropdown_pin': dropdown_pin,
+                        'dropdown_stat': dropdown_stat
+                    });
+                }
+            } else {
+                Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+                    'dropdown_pin': dropdown_pin,
+                    'dropdown_stat': dropdown_stat
+                });
+            }
+            code += JST['zum_led']({
                 'dropdown_pin': dropdown_pin,
                 'dropdown_stat': dropdown_stat
             });
-
-            var code = JST['zum_led']({
-                'dropdown_pin': dropdown_pin,
-                'dropdown_stat': dropdown_stat
-            });
-
             return code;
         };
-
         /**
          * zum_led block definition
          * @type {Object}
@@ -8998,23 +9014,16 @@
              */
             init: function() {
                 this.setColour(RoboBlocks.LANG_COLOUR_ZUM);
-                this.appendValueInput('PIN')
-                    .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED'))
-                    .appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom))
-                    .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_PIN'));
-                this.appendDummyInput()
-                    .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_STATE'))
-                    .setAlign(Blockly.ALIGN_RIGHT)
-                    .appendField(new Blockly.FieldDropdown([
-                        [RoboBlocks.locales.getKey('LANG_ZUM_LED_ON') || 'ON', 'HIGH'],
-                        [RoboBlocks.locales.getKey('LANG_ZUM_LED_OFF') || 'OFF', 'LOW']
-                    ]), 'STAT');
+                this.appendValueInput('PIN').appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED')).appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom)).appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_PIN'));
+                this.appendDummyInput().appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_STATE')).setAlign(Blockly.ALIGN_RIGHT).appendField(new Blockly.FieldDropdown([
+                    [RoboBlocks.locales.getKey('LANG_ZUM_LED_ON') || 'ON', 'HIGH'],
+                    [RoboBlocks.locales.getKey('LANG_ZUM_LED_OFF') || 'OFF', 'LOW']
+                ]), 'STAT');
                 this.setPreviousStatement(true, null);
                 this.setNextStatement(true, null);
                 this.setTooltip(RoboBlocks.locales.getKey('LANG_ZUM_LED_TOOLTIP'));
             }
         };
-
         // Source: src/blocks/zum_photoresistor/zum_photoresistor.js
         /* global Blockly, options, JST, RoboBlocks */
         /* jshint sub:true */
--- src/blocks/zum_led/zum_led.js
+++ src/blocks/zum_led/zum_led.js
@@ -1,29 +1,45 @@
 'use strict';
 /* global Blockly, options, JST, RoboBlocks */
 /* jshint sub:true */
-
 /**
  * zum_led code generation
  * @return {String} Code generated with block parameters
  */
 Blockly.Arduino.zum_led = function() {
-    var dropdown_pin = Blockly.Arduino.valueToCode(this,'PIN', Blockly.Arduino.ORDER_ATOMIC)||'';
+    var dropdown_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC) || '';
     var dropdown_stat = this.getFieldValue('STAT');
-
-
-    Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+    var code = '';
+    if (this.childBlocks_ !== undefined) {
+        var pin_block=[];
+        for (var i in this.childBlocks_){
+            if (this.childBlocks_[i].type==='variables_get' || this.childBlocks_[i].type==='math_number'){
+                pin_block.push(this.childBlocks_[i].type);
+            }
+        }
+        if (pin_block[0] === 'variables_get') {
+            code += JST['zum_led_setups']({
+                'dropdown_pin': dropdown_pin,
+                'dropdown_stat': dropdown_stat
+            });
+        }
+        if (pin_block[0] === 'math_number') {
+            Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+                'dropdown_pin': dropdown_pin,
+                'dropdown_stat': dropdown_stat
+            });
+        }
+    } else {
+        Blockly.Arduino.setups_['setup_green_led_' + dropdown_pin] = JST['zum_led_setups']({
+            'dropdown_pin': dropdown_pin,
+            'dropdown_stat': dropdown_stat
+        });
+    }
+    code += JST['zum_led']({
         'dropdown_pin': dropdown_pin,
         'dropdown_stat': dropdown_stat
     });
-
-    var code = JST['zum_led']({
-        'dropdown_pin': dropdown_pin,
-        'dropdown_stat': dropdown_stat
-    });
-
     return code;
 };
-
 /**
  * zum_led block definition
  * @type {Object}
@@ -31,23 +47,17 @@ Blockly.Arduino.zum_led = function() {
 Blockly.Blocks.zum_led = {
     category: RoboBlocks.locales.getKey('LANG_CATEGORY_ZUM'),
     tags: ['bq', 'led'],
-    helpUrl: RoboBlocks.GITHUB_SRC_URL+'blocks/zum_led',
+    helpUrl: RoboBlocks.GITHUB_SRC_URL + 'blocks/zum_led',
     /**
-    * zum_led initialization
-    */
+     * zum_led initialization
+     */
     init: function() {
         this.setColour(RoboBlocks.LANG_COLOUR_ZUM);
-        this.appendValueInput('PIN')
-            .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED'))
-            .appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom))
-            .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_PIN'));
-        this.appendDummyInput()
-            .appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_STATE'))
-            .setAlign(Blockly.ALIGN_RIGHT)
-            .appendField(new Blockly.FieldDropdown([
-                [RoboBlocks.locales.getKey('LANG_ZUM_LED_ON')||'ON', 'HIGH'],
-                [RoboBlocks.locales.getKey('LANG_ZUM_LED_OFF')||'OFF', 'LOW']
-            ]), 'STAT');
+        this.appendValueInput('PIN').appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED')).appendField(new Blockly.FieldImage('img/blocks/zum04.png', 208 * options.zoom, 140 * options.zoom)).appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_PIN'));
+        this.appendDummyInput().appendField(RoboBlocks.locales.getKey('LANG_ZUM_LED_STATE')).setAlign(Blockly.ALIGN_RIGHT).appendField(new Blockly.FieldDropdown([
+            [RoboBlocks.locales.getKey('LANG_ZUM_LED_ON') || 'ON', 'HIGH'],
+            [RoboBlocks.locales.getKey('LANG_ZUM_LED_OFF') || 'OFF', 'LOW']
+        ]), 'STAT');
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setTooltip(RoboBlocks.locales.getKey('LANG_ZUM_LED_TOOLTIP'));
