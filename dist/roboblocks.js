f798d6dd15f16ae22a615bbc9b462bfbcc308b22
Irene Sanz <irene.sanz@bq.com>
2015-01-08 10:29:14 +0100

    modified zum_button to put the pinmode in the loop when using a variable to define the pins

--- dist/roboblocks.js
+++ dist/roboblocks.js
@@ -3344,7 +3344,7 @@
             with(obj) {
                 __p += 'digitalRead(' +
                     __e(dropdown_pin) +
-                    ')\n';
+                    ')';
 
             }
             return __p
@@ -8766,25 +8766,34 @@
         // Source: src/blocks/zum_button/zum_button.js
         /* global Blockly, options, JST, RoboBlocks */
         /* jshint sub:true */
-
         /**
          * zum_button code generation
          * @return {String} Code generated with block parameters
          */
         Blockly.Arduino.zum_button = function() {
             var dropdown_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
-
-            Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+            var code = '';
+            if (this.childBlocks_ !== undefined) {
+                var pin_block = this.childBlocks_[0].type;
+                if (pin_block === 'variables_get') {
+                    code += JST['zum_button_setups']({
+                        'dropdown_pin': dropdown_pin,
+                    });
+                } else if (pin_block === 'math_number') {
+                    Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+                        'dropdown_pin': dropdown_pin,
+                    });
+                }
+            } else {
+                Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+                    'dropdown_pin': dropdown_pin,
+                });
+            }
+            code += JST['zum_button']({
                 'dropdown_pin': dropdown_pin,
             });
-
-            var code = JST['zum_button']({
-                'dropdown_pin': dropdown_pin,
-            });
-            code = 'digitalRead(' + dropdown_pin + ')';
             return [code, Blockly.Arduino.ORDER_ATOMIC];
         };
-
         /**
          * zum_button block definition
          * @type {Object}
@@ -8798,15 +8807,11 @@
              */
             init: function() {
                 this.setColour(RoboBlocks.LANG_COLOUR_ZUM);
-                this.appendValueInput('PIN')
-                    .appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON'))
-                    .appendField(new Blockly.FieldImage('img/blocks/zum02.png', 212 * options.zoom, 139 * options.zoom))
-                    .appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_PIN'));
+                this.appendValueInput('PIN').appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON')).appendField(new Blockly.FieldImage('img/blocks/zum02.png', 212 * options.zoom, 139 * options.zoom)).appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_PIN'));
                 this.setOutput(true, Boolean);
                 this.setTooltip(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_TOOLTIP'));
             }
         };
-
         // Source: src/blocks/zum_follower/zum_follower.js
         /* global Blockly, options, JST, RoboBlocks */
         /* jshint sub:true */
--- src/blocks/zum_button/zum_button.js
+++ src/blocks/zum_button/zum_button.js
@@ -1,25 +1,34 @@
 'use strict';
 /* global Blockly, options, JST, RoboBlocks */
 /* jshint sub:true */
-
 /**
  * zum_button code generation
  * @return {String} Code generated with block parameters
  */
 Blockly.Arduino.zum_button = function() {
-    var dropdown_pin = Blockly.Arduino.valueToCode(this,'PIN', Blockly.Arduino.ORDER_ATOMIC);
-
-    Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+    var dropdown_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+    var code = '';
+    if (this.childBlocks_ !== undefined) {
+        var pin_block = this.childBlocks_[0].type;
+        if (pin_block === 'variables_get') {
+            code += JST['zum_button_setups']({
+                'dropdown_pin': dropdown_pin,
+            });
+        } else if (pin_block === 'math_number') {
+            Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+                'dropdown_pin': dropdown_pin,
+            });
+        }
+    } else {
+        Blockly.Arduino.setups_['setup_button_' + dropdown_pin] = JST['zum_button_setups']({
+            'dropdown_pin': dropdown_pin,
+        });
+    }
+    code += JST['zum_button']({
         'dropdown_pin': dropdown_pin,
     });
-
-    var code = JST['zum_button']({
-        'dropdown_pin': dropdown_pin,
-    });
-    code = 'digitalRead(' + dropdown_pin + ')';
     return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
-
 /**
  * zum_button block definition
  * @type {Object}
@@ -27,16 +36,13 @@ Blockly.Arduino.zum_button = function() {
 Blockly.Blocks.zum_button = {
     category: RoboBlocks.locales.getKey('LANG_CATEGORY_ZUM'),
     tags: ['bq', 'botón'],
-    helpUrl: RoboBlocks.GITHUB_SRC_URL+'blocks/zum_button',
+    helpUrl: RoboBlocks.GITHUB_SRC_URL + 'blocks/zum_button',
     /**
-    * zum_button initialization
-    */
+     * zum_button initialization
+     */
     init: function() {
         this.setColour(RoboBlocks.LANG_COLOUR_ZUM);
-        this.appendValueInput('PIN')
-            .appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON'))
-            .appendField(new Blockly.FieldImage('img/blocks/zum02.png', 212 * options.zoom, 139 * options.zoom))
-            .appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_PIN'));
+        this.appendValueInput('PIN').appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON')).appendField(new Blockly.FieldImage('img/blocks/zum02.png', 212 * options.zoom, 139 * options.zoom)).appendField(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_PIN'));
         this.setOutput(true, Boolean);
         this.setTooltip(RoboBlocks.locales.getKey('LANG_ZUM_BUTTON_TOOLTIP'));
     }
